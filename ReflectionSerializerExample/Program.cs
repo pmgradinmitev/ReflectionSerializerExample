32a0e4840eceb238a7077dc2eb5061e12cfa12b7
pmg.tadinmitev92 <pmg.tadinmitev92@gmail.com>
2025-01-30 09:48:27 +0200

    Add project files.

---
+++ ReflectionSerializerExample/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReflectionSerializerExample;
+
+var person = new Person
+{
+    Name = "John",
+    Age = 30,
+    Address = new Address
+    {
+        Street = "123 Main St",
+        City = "New York"
+    }
+};
+
+var dictionary = ReflectionSerializer.Serialize(person);
+var json = ReflectionSerializer.ConvertToJson(dictionary);
+Console.WriteLine(json);
---
+++ ReflectionSerializerExample/ReflectionSerializer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ReflectionSerializerExample
+{
+    internal class ReflectionSerializer
+    {
+        public static object Serialize(object obj)
+        {
+            // Handle null case
+            if (obj == null)
+                return "null";
+
+            var result = new Dictionary<string, object>();
+
+            // Get the type of the object
+            var type = obj.GetType();
+
+            // Get all properties of the object
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var property in properties)
+            {
+                // Get the value of the property
+                var value = property.GetValue(obj);
+
+                // Recursively serialize the value if it's a complex object, otherwise just add it to the dictionary
+                if (value != null && !property.PropertyType.IsPrimitive && property.PropertyType != typeof(string))
+                {
+                    result[property.Name] = Serialize(value); // Call serialize recursively
+                }
+                else
+                {
+                    result[property.Name] = value;
+                }
+            }
+          
+            return result;
+        }
+
+        public static string ConvertToJson(object obj)
+        {
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+            return System.Text.Json.JsonSerializer.Serialize(obj, options);
+        }
+    }
+}
